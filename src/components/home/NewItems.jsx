861a85394c71f4b1fdba7bbccb7ee5eb682eddd3
Nadia <nh894@nyu.edu>
2024-10-01 17:06:43 -0400

    explore items data dynamically fetched & displayed as reusable component NftItem, load more button functionality implemented, loading state added, filter functionality implemented

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import OwlCarousel from "react-owl-carousel";
 import CountdownTimer from "../UI/CountdownTimer";
+import NftItem from "../UI/NftItem";
 
 const NewItems = () => {
   const [newItems, setNewItems] = useState([]);
@@ -96,60 +97,7 @@ const NewItems = () => {
           ) : (
             <OwlCarousel className="owl-carousel" {...options}>
               {newItems.map((nft) => (
-                <div className="nft__item" key={nft.id}>
-                  <div className="author_list_pp">
-                    <Link
-                      to="/author"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title={`Creator: ${nft.authorName}`}
-                    >
-                      <img className="lazy" src={nft.authorImage} alt="" />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="de_countdown">
-                    <CountdownTimer expiryDate={nft.expiryDate} />
-                  </div>
-
-                  <div className="nft__item_wrap">
-                    <div className="nft__item_extra">
-                      <div className="nft__item_buttons">
-                        <button>Buy Now</button>
-                        <div className="nft__item_share">
-                          <h4>Share</h4>
-                          <a href="" target="_blank" rel="noreferrer">
-                            <i className="fa fa-facebook fa-lg"></i>
-                          </a>
-                          <a href="" target="_blank" rel="noreferrer">
-                            <i className="fa fa-twitter fa-lg"></i>
-                          </a>
-                          <a href="">
-                            <i className="fa fa-envelope fa-lg"></i>
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-
-                    <Link to="/item-details">
-                      <img
-                        src={nft.nftImage}
-                        className="lazy nft__item_preview"
-                        alt=""
-                      />
-                    </Link>
-                  </div>
-                  <div className="nft__item_info">
-                    <Link to="/item-details">
-                      <h4>{nft.title}</h4>
-                    </Link>
-                    <div className="nft__item_price">{nft.price} ETH</div>
-                    <div className="nft__item_like">
-                      <i className="fa fa-heart"></i>
-                      <span>{nft.likes}</span>
-                    </div>
-                  </div>
-                </div>
+                <NftItem id={nft.id} authorName={nft.authorName} authorImage={nft.authorImage} expiryDate={nft.expiryDate} nftImage={nft.nftImage} price={nft.price} title={nft.title} likes={nft.likes} />
               ))}
             </OwlCarousel>
           )}
